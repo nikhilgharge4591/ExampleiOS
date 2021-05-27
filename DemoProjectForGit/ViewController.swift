8b398c0c8f54d8bc08ea5aae35c46033122a7c86
Nikhil Gharge <61109207+nikhilgharge4591@users.noreply.github.com>
2021-05-27 19:34:30 +0100

    Complete Feature 1

--- DemoProjectForGit/ViewController.swift
+++ DemoProjectForGit/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func helloButton_Clicked(_ sender: UIButton) {
+        greetingLabel.text = "Hello, \(nameTextField.text)"
+    }
+    
 }
 
